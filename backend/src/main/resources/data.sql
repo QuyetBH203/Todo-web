9f3eb741c61ce19f5f23591052bbafa998aeac44
QuyetCN05 <buihuuquyet203@gmail.com>
2024-05-05 17:19:29 +0700

    first commit

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into todo(done,id,target_date,description,username) values
+(false,1001,CURRENT_DATE(),'Learn AWS','in28minutes'),
+(false,1002,CURRENT_DATE(),'Learn Devops','in28minutes'),
+(false,1003,CURRENT_DATE(),'Learn AI','in28minutes');
